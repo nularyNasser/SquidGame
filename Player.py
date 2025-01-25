336e5c28a0033b335006c2c7066dc6ca17bfd6c6
nularyNasser <137123083+nularyNasser@users.noreply.github.com>
2025-01-25 23:10:18 +0100

    Créaton de la class Player.py et teste dans le fichier main.py

---
+++ Player.py
@@ -0,0 +1,13 @@
+class Player:
+
+    def __init__(self, nb_bille):
+        self.nb_bille = nb_bille
+        self.nb_bille_parie = 0
+
+    def get_nb_bille(self): return self.nb_bille
+    
+    def get_nb_bille_parie(self): return self.nb_bille_parie
+    
+    def parier(self, nb_bille: int):
+        assert nb_bille <= self.nb_bille
+        self.nb_bille_parie = nb_bille
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+from Player import Player
+from random import randint
+
+
+player = Player(nb_bille=10)
+bot = Player(nb_bille=10)
+
+print(player.get_nb_bille())
+print(bot.get_nb_bille())
