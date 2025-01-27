b5b700f36a7fcc1a1e17328a622b2f2ed4e995b5
nularyNasser <nahamed.sveil@gmail.com>
2025-01-27 01:44:49 +0100

    Ajout de la bibliotheque doctest

--- Player.py
+++ Player.py
@@ -1,10 +1,13 @@
+
+from doctest import testmod
 class Player:
 
     def __init__(self, nb_bille):
         self.nb_bille = nb_bille
         self.nb_bille_parie = 0
 
-    def get_nb_bille(self): return self.nb_bille
+    def get_nb_bille(self):
+        return self.nb_bille
     
     def get_nb_bille_parie(self): return self.nb_bille_parie
     
